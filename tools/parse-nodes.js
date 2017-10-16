15dc805714ff2c2b0dac05afd862117994e3c990
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2017-10-16 13:19:00 +0300

    Generating a new nodes.json file

---
+++ tools/parse-nodes.js
@@ -0,0 +1,55 @@
+const fs = require('fs'),
+  path = require('path');
+
+if (process.argv.length < 3) {
+  throw new Error("Specify path to a Ansible's inventory file (/full/path/to/inventory/waves/devnet)");
+}
+
+const file = fs.readFileSync(process.argv[2], {encoding: 'UTF-8'}),
+  lines = file.split('\n'),
+  terraformSectionIndex = lines.indexOf('[aws-terraform]');
+
+const regexp = /^(.+)\s+node_id=(-?\d+)/;
+let nodes = [
+  {
+    rest: '34.251.200.245:6869',
+    nodeId: 0
+  },
+  {
+    rest: '34.251.200.245:16869',
+    nodeId: 1
+  },
+  {
+    rest: '35.157.212.173:6869',
+    nodeId: 2
+  },
+  {
+    rest: '35.157.212.173:16869',
+    nodeId: 3
+  },
+  {
+    rest: '13.229.61.140:6869',
+    nodeId: 4
+  },
+  {
+    rest: '13.229.61.140:16869',
+    nodeId: 5
+  }
+];
+
+for (let i = terraformSectionIndex + 1; i <= lines.length; i++) {
+  const match = regexp.exec(lines[i]);
+  if (match) {
+    nodes.push({
+      rest: match[1].trim() + ':6869',
+      nodeId: +match[2]
+    });
+  }
+}
+
+const rootDir = path.normalize(path.dirname(process.argv[1]) + '/..'),
+  destFile = rootDir + '/data/nodes.json';
+
+fs.writeFileSync(destFile, JSON.stringify(nodes, null, 2), {encoding: 'UTF-8'});
+console.log('New nodes are written to ' + destFile + ':');
+console.log(nodes);
