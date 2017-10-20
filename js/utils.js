d9817e29e680e76c90a8a0184aa03d3eaa062e93
Vyatcheslav Suharnikov <arz.freezy@gmail.com>
2017-10-20 10:47:58 +0300

    Show nodeId with a link to the REST

--- js/utils.js
+++ js/utils.js
@@ -30,3 +30,11 @@ window.Utils = {
     });
   }
 };
+
+Handlebars.registerHelper('if_eq', function(a, b, opts) {
+  if (a === b) {
+    return opts.fn(this);
+  } else {
+    return opts.inverse(this);
+  }
+});
